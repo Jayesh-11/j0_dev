b562ac945fef58f7da4d721bbd9afcfa317331cc
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2025-10-14 02:55:25 +0530

    added mdx support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from "react-router";
 import Home from "./components/Home/Home";
+import BlogList from "./components/Blog/BlogList.mdx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<BlogList />} />
       </Routes>
     </BrowserRouter>
   );
