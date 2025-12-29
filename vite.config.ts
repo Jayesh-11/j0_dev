6b333287cae859a7d8a8091c5a9c725a2a99de2c
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2025-12-29 21:16:13 +0530

    boom

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
-import mdx from "@mdx-js/rollup";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [mdx(), react(), tailwindcss()],
-});
